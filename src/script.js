8a67ffc2fde6b8626dff9190024094b7227c5aab
Prashant <prashant.patel77@gmail.com>
2020-09-27 16:37:14 +1000

    Added function to list the Gifs

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import Amplify, { API, graphqlOperation } from "@aws-amplify/api";
 import awsConfig from "./aws-exports";
 import { createGiphme } from "./graphql/mutations";
+import { listGiphmes } from "./graphql/queries";
 
 Amplify.configure(awsConfig);
 
@@ -21,6 +22,10 @@ const createNewGif = async (e) => {
     );
     // Print the data to the console once it comes back
     console.log(newGif);
+
+    //get all gifs
+    getGifs();
+
     // Reset the form (make the fields blank again)
     document.getElementById("create-form").reset();
   } catch (err) {
@@ -31,3 +36,28 @@ const createNewGif = async (e) => {
 
 // run our createNewGif function when the form is submitted
 document.getElementById("create-form").addEventListener("submit", createNewGif);
+
+const getGifs = async () => {
+  // select the container element
+  const container = document.querySelector(".container");
+  // reset its current contents
+  container.innerHTML = "";
+  // make a request to get all our gifs
+  const gifs = await API.graphql(graphqlOperation(listGiphmes));
+
+  // loop through our gifs and
+  gifs.data.listGiphmes.items.map((gif) => {
+    // create a new image element
+    const img = document.createElement("img");
+    // add the src attribute to the img
+    img.setAttribute("src", gif.url);
+    // add the alt attribute to the img
+    img.setAttribute("alt", gif.altText);
+
+    img.setAttribute("width", "25%");
+    // add the image to the container
+    document.querySelector(".container").appendChild(img);
+  });
+};
+
+getGifs();
